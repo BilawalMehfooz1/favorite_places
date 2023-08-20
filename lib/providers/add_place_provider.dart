24b634d8475be6d5a1c2a5a2b4a91985133d7f29
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-20 21:59:18 +0500

    creating database for storing data in memory

--- lib/providers/add_place_provider.dart
+++ lib/providers/add_place_provider.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favorite_places/models/places_structure.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class AddPlaceNotifier extends StateNotifier<List<PlacesStructure>> {
   AddPlaceNotifier() : super(const []);
@@ -17,6 +19,28 @@ class AddPlaceNotifier extends StateNotifier<List<PlacesStructure>> {
       image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY,title TEXT,image TEXT,lat REAL,lng REAL,address TEXT)');
+      },
+      version: 1,
+    );
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitute,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.longitude,
+      },
+    );
+
     state = [newPlace, ...state];
   }
 }
