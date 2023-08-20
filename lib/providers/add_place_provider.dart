14d275a140dc292aaa88643237386405d501184e
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-20 18:47:22 +0500

    saving Image in memory

--- lib/providers/add_place_provider.dart
+++ lib/providers/add_place_provider.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favorite_places/models/places_structure.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class AddPlaceNotifier extends StateNotifier<List<PlacesStructure>> {
   AddPlaceNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
     final newPlace = PlacesStructure(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
     state = [newPlace, ...state];
